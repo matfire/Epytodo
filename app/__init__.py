25e13b5a6efea8036fa93c252a94160500b3c8c6
Matteo Gassend <matteo.gassend@epitech.eu>
2018-04-06 17:35:44 +0200

    modified navbar to show log button when user not logged and viceversa

--- app/__init__.py
+++ app/__init__.py
@@ -5,8 +5,8 @@ from flask import Flask
 app = Flask(__name__)
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'Galako99!!'
-app.config['MYSQL_DB'] = 'Epytodo'
+app.config['MYSQL_PASSWORD'] = 'root'
+app.config['MYSQL_DB'] = 'epytodo'
 app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
 
 
